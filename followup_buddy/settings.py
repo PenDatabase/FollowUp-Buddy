81cf4f08f0d581f3cb690888ebdc6daeb577c3c9
PenDatabase <davideneasatochibueze@gmail.com>
2025-07-22 02:53:32 +0100

    feat: made csrf tokens to be trusted

--- followup_buddy/settings.py
+++ followup_buddy/settings.py
@@ -164,8 +164,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
     SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_REDIRECT_EXEMPT = []
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    CSRF_TRUSTED_ORIGINS = [
+        'https://*.railway.app',
+    ]
